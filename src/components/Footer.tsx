2cd5ebbfbdb365931fd089af08d3840b926991ad
ze-codes <solidman8888@gmail.com>
2025-04-28 19:24:40 +0800

    small fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Ze Chen. All rights reserved.</p>
+      <p>&copy; 2025 Ze Chen.</p>
     </footer>
   );
 }
